87e5e4c04959700b095ae5c7d83b354b3066227a
Shivendra singh <singhanshul88508@gmail.com>
2026-08-29 22:43:42 +0530

    Added deletion operations on linkedlist

--- Java journey/LinkedList.java
+++ Java journey/LinkedList.java
@@ -15,11 +15,9 @@ class Node {
 
 
 
-
-
 public class LinkedList {
 
-     private static Node arrtoLL (int[] arr) {
+    private static Node arrtoLL (int[] arr) {
         Node head = new Node(arr[0]);
         Node mover = head;
         for (int i = 1; i < arr.length; i++) {
@@ -28,10 +26,11 @@ public class LinkedList {
             mover = temp;
         }
         
-    return head; }
+    return head; 
+}
 
     private static void printLL(Node head) {
-         while(head != null) {
+        while(head != null) {
             System.out.print(head.data + " ");
             head = head.next;
         }
@@ -45,7 +44,8 @@ public class LinkedList {
             temp = temp.next;
             cnt++;
         }
-    return cnt;}
+        return cnt;
+    }
 
     private static boolean searchValue(Node head, int val) {
         Node temp = head;
@@ -53,23 +53,47 @@ public class LinkedList {
             if(temp.data == val) return true;
             temp = temp.next;
         }
-   return false; }
-
-   private static Node removeHead(Node head) {
-    if(head == null) return head;
-    head = head.next;
-
-   return head;}
-
-   private static Node removeTail(Node head) {
-    if(head == null) return head;
-    Node temp = head;
-    while( temp.next.next != null) {
-        temp = temp.next;
+        return false; 
     }
-    temp.next = null;
 
-   return head;}
+    private static Node removeHead(Node head) {
+        if(head == null) return head;
+        head = head.next;
+    return head;
+    }
+
+    private static Node removeTail(Node head) {
+        if(head == null) return head;
+        Node temp = head;
+        while( temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+
+        return head;
+    }
+
+    private static Node removekthNode(Node head, int k) {
+        if(head == null ) return head;
+        if(k == 1) {
+            head = head.next;
+        }
+        int cnt = 0; 
+        Node temp = head;
+        Node prev = null;
+        while (temp != null) {
+            cnt++;
+            if(cnt == k) {
+                
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+
+        }
+
+    return head;}
 
 
 
@@ -79,9 +103,11 @@ public class LinkedList {
        
         System.out.println("Length of LL: " + lengthOfLL(head));
         System.out.println(searchValue(head,1)); 
-        head = removeHead(head);
-        printLL(head);
-        head = removeTail(head);
+        // head = removeHead(head);
+        // printLL(head);
+        // head = removeTail(head);
+        // printLL(head);
+        head = removekthNode(head, 2);
         printLL(head);
     }
    
